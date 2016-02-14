ea9d694e7875cb00c403baa7e9bc04bd528f2d8e
Max <maksymkharuk@iesebre.com>
2016-02-14 21:26:21 +0100

    Implemented notifications with onesignal API, added moment lib, added new features to chat

--- src/components/Pushwoosh.vue
+++ src/components/Pushwoosh.vue
@@ -7,7 +7,7 @@
                 <h3 class="box-title">Direct Chat</h3>
 
                 <div class="box-tools pull-right">
-                    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-green">3</span>
+                    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-green">{{total}}</span>
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                     </button>
                     <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
@@ -28,6 +28,9 @@
                         <img class="direct-chat-img" src="bower_components/AdminLTE/dist/img/user1-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                             {{ notification.message }}
+                            <span class="pull-right">
+                                <button v-on:click="removeNotification(notification)" type="button" class="btn btn-box-tool"><i class="fa fa-minus"></i></button>
+                            </span>
                         </div>
                         <!-- /.direct-chat-text -->
                     </div>
@@ -40,10 +43,13 @@
             <!-- /.box-body -->
             <div class="box-footer">
                 <div class="input-group">
-                    <input id="message" type="text" name="message" placeholder="Type Message ..." class="form-control">
-                  <span class="input-group-btn">
-                    <button v-on:click="notify" type="button" class="btn btn-success btn-flat">Send</button>
-                  </span>
+                    <input id="message" type="text" name="message" placeholder="Type Message ..." class="form-control"
+                           v-model="notificationMessage"
+                           @keyup.enter="addNotification">
+                              <span class="input-group-btn">
+                                  <button v-on:click="addNotification" type="button" class="btn btn-success btn-flat">Send</button>
+                                  <button v-on:click="clear" type="button" class="btn btn-danger btn-flat">Clear</button>
+                              </span>
                 </div>
             </div>
             <!-- /.box-footer-->
@@ -53,40 +59,96 @@
 </template>
 
 <script>
+
+    import Vue from 'vue'
+    import moment from 'moment';
+
     export default {
         data () {
             return {
                 msg: 'Hello World!',
-                notification_message: 'Jolo Compolo',
+                notificationMessage: '',
                 notifications: notificationStorage.fetch()
             }
         },
+        computed: {
+            total: function () {
+                return this.notifications.length;
+            }
+        },
+        watch: {
+            notifications: {
+                handler: function (notifications) {
+                    notificationStorage.save(notifications);
+                },
+                deep: true
+            }
+        },
         methods: {
-            notify: function() {
+            addNotification: function() {
+                var value = this.notificationMessage && this.notificationMessage.trim();
 
-                var notification = {message:$('#message').val(), date:"Now"};
-                this.notifications.push(notification);
+                if (!value) { return; }
 
-                notificationStorage.save(this.notifications);
+                this.notifications.push({ message: value, date: moment().calendar()});
+                this.notifyWithAjax();
 
+                this.notificationMessage = '';
+            },
+            removeNotification: function (notification) {
+                this.notifications.$remove(notification);
+            },
+            clear: function () {
+                this.notifications = [];
+                notificationStorage.save([]);
+            },
+            notifyWithAjax: function() {
+
+                //var notification = {message:$('#message').val(), date:"Now"};
+                //this.notifications.push(notification);
+
+                //notificationStorage.save(this.notifications);
+
+                //Pushwoosh
+//                $.ajax({
+//                    type: "POST",
+//                    url: "https://cp.pushwoosh.com/json/1.3/createMessage",
+//                    data: JSON.stringify({
+//                        "request": {
+//                            "application": "4FC89B6D14A655.46488481",
+//                            "auth": "mTdns0j6qLYPa/A5htmD46xVyoxdVQfPBz7NRqYYHz9PhvKXgJtOkAY+yo0YTXDEoztQAJFY0JmXnd89tf59",
+//                            "notifications": [{
+//                                "send_date": "now",
+//                                "ignore_user_timezone": true,
+//                                "content": this.notificationMessage
+//                            }]
+//                        }
+//                    }),
+//                    dataType: "json"
+//                }).done(function(data) {
+//                    console.log(data);
+//                });
+
+                // OneSignal
                 $.ajax({
                     type: "POST",
-                    url: "https://cp.pushwoosh.com/json/1.3/createMessage",
+                    url: "https://onesignal.com/api/v1/notifications",
+                    headers: {
+                        "Authorization": "Basic OWI2OGE1MDctYWFlZC00ZGYwLTkxOTMtZDAyOWJhNWIwYjFk",
+                        "Content-Type": "application/json"
+                    },
                     data: JSON.stringify({
-                        "request": {
-                            "application": "4FC89B6D14A655.46488481",
-                            "auth": "mTdns0j6qLYPa/A5htmD46xVyoxdVQfPBz7NRqYYHz9PhvKXgJtOkAY+yo0YTXDEoztQAJFY0JmXnd89tf59",
-                            "notifications": [{
-                                "send_date": notification.date,
-                                "ignore_user_timezone": true,
-                                "content": notification.message
-                            }]
-                        }
-                    }),
-                    dataType: "json"
+                        "app_id": "1612eca0-0bab-4126-bbfa-bcc8c341c036",
+                        "included_segments": ["All"],
+                        "data": {"foo": "bar"},
+                        "isAndroid":true,
+                        "contents": {"en": this.notificationMessage,"es": this.notificationMessage},
+                        "headings": {"en": "Wake up!","es": "Despierta!"}
+                    })
                 }).done(function(data) {
                     console.log(data);
                 });
+
             }
         }
     }
